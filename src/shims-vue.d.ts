02b3db3dfb60f7b1d620dd8611a3e8b4d83cdcb0
DamirAid <damir.redmouse@gmail.com>
2023-03-16 02:25:29 +0600

    some fixes

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module '*.svg' {
+  import Vue, {VueConstructor} from 'vue';
+  const content: VueConstructor<Vue>;
+  export default content;
+}
